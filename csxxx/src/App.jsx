fd6e0e678f5d163bd8623af5d0ed34eb2b3e527a
Moe Amadou <moeamadou@Moes-MacBook-Pro.local>
2022-11-01 17:23:02 -0400

    Convert to class component to fix timer state

--- csxxx/src/App.jsx
+++ csxxx/src/App.jsx
@@ -4,7 +4,7 @@ import logo from './transit-logo.png';
 import barcode from './barcode.png';
 import arrow from './arrow.png';
 import divider from './divider.png';
-import { useState } from 'react';
+import React from 'react';
 
 const ticketNumber = "MZ56120152"
 const from = "Union Station Bus Terminal"
@@ -13,75 +13,91 @@ const four_hours_as_seconds = 4 * 60 * 60;
 const one_hour_as_seconds = 60 * 60;
 const seed = Math.floor(Math.random() * one_hour_as_seconds);
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(dayjs().format('MMM DD YYYY, hh:mm:ss A'))
-  const [validityTimer, setValidityTimer] = useState(seed);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: dayjs().format('MMM DD YYYY, hh:mm:ss A'),
+      validityTimer: seed
+    };
+  }
 
-  setInterval(function() {
-    setCurrentTime(dayjs().format('MMM DD YYYY, hh:mm:ss A'))
-    setValidityTimer(validityTimer >= four_hours_as_seconds? seed : validityTimer + 1)
-  }, 1000);
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      console.log('interval hit');
+      this.setState({
+        currentTime: dayjs().format('MMM DD YYYY, hh:mm:ss A'),
+        validityTimer: this.state.validityTimer >= four_hours_as_seconds? seed : this.state.validityTimer + 1,
+      });
+    }, 1000);
+  }
 
-  return (
-    <div className="app_container">
-      <div className="header green--flashing">
-        <img className="header__gt-icon" src={logo}/>
-        <div className="header__heading__scroll-container">
-            <div className="header__heading--animated">GO TRANSIT</div>
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-            <div className="header__heading--animated2">·</div>
+  render() {
+    return (
+      <div className="app_container">
+        <div className="header green--flashing">
+          <img className="header__gt-icon" src={logo}/>
+          <div className="header__heading__scroll-container">
+              <div className="header__heading--animated">GO TRANSIT</div>
 
-            <div className="header__heading--animated3">GO TRANSIT</div>
+              <div className="header__heading--animated2">·</div>
 
-            <div className="header__heading--animated4">·</div>
+              <div className="header__heading--animated3">GO TRANSIT</div>
+
+              <div className="header__heading--animated4">·</div>
+          </div>
+          <div className="header__itinerary">
+            <span>{from}</span> to <span>{destination}</span>
+          </div>
         </div>
-        <div className="header__itinerary">
-          <span>{from}</span> to <span>{destination}</span>
+
+        <div className="body">
+          <div className="body__circle"/>
+          <div className="body__trip-info">
+            <div className="body__trip-info__passenger-info">
+              <span className="body__trip-info__passenger-info__num-passengers">x1</span>
+              <span className="body__trip-info__passenger-info__copy">Passenger(s)</span>
+              <span className="body__trip-info__passenger-info__passenger-type">1x Adult</span>
+            </div>
+            <img className="body__trip-info__vertical-divider" src={divider} />
+            <div className="body__trip-info__trip-type">
+              <img className="body__trip-info__trip-type__arrow" src={arrow}/>
+              <span className="body__trip-info__trip-type__copy">One-Way</span>
+            </div>
+          </div>
+
+          <div className="body__horizontal-divider" />
+
+          <span className="body__ticket-number">Ticket Number: <b>{ticketNumber}</b></span>
+          <div className="body__barcode">
+          < img className="body__barcode__outline" src={barcode}/>
+            <div className="body__barcode--animated">VALID FOR TRAVEL</div>
+          </div>
+          <div className="body__countdown-container">
+            <div className="body__countdown-container__current-time">
+              <span className="body__countdown-container__current-time__heading">CURRENT TIME:</span>
+              <span className="body__countdown-container__current-time__clock">{this.state.currentTime}</span>
+            </div>
+
+            <div className="body__countdown-container__time-since-activation">
+              <span className="body__countdown-container__time-since-activation__heading">TIME SINCE ACTIVATION:</span>
+              <span className="body__countdown-container__time-since-activation__clock">{formatTime(this.state.validityTimer)}</span>
+            </div>
+          </div>
+        </div>
+
+
+        <div id="footer" className="footer green--flashing">
+          <span className="footer__instruction">Please show this to the proper authority on board the train.</span>
+          <span className="footer__hourglass">{formatTime(four_hours_as_seconds - this.state.validityTimer)}</span>
         </div>
       </div>
-
-      <div className="body">
-        <div className="body__circle"/>
-        <div className="body__trip-info">
-          <div className="body__trip-info__passenger-info">
-            <span className="body__trip-info__passenger-info__num-passengers">x1</span>
-            <span className="body__trip-info__passenger-info__copy">Passenger(s)</span>
-            <span className="body__trip-info__passenger-info__passenger-type">1x Adult</span>
-          </div>
-          <img className="body__trip-info__vertical-divider" src={divider} />
-          <div className="body__trip-info__trip-type">
-            <img className="body__trip-info__trip-type__arrow" src={arrow}/>
-            <span className="body__trip-info__trip-type__copy">One-Way</span>
-          </div>
-        </div>
-
-        <div className="body__horizontal-divider" />
-
-        <span className="body__ticket-number">Ticket Number: <b>{ticketNumber}</b></span>
-        <div className="body__barcode">
-        < img className="body__barcode__outline" src={barcode}/>
-          <div className="body__barcode--animated">VALID FOR TRAVEL</div>
-        </div>
-        <div className="body__countdown-container">
-          <div className="body__countdown-container__current-time">
-            <span className="body__countdown-container__current-time__heading">CURRENT TIME:</span>
-            <span className="body__countdown-container__current-time__clock">{currentTime}</span>
-          </div>
-
-          <div className="body__countdown-container__time-since-activation">
-            <span className="body__countdown-container__time-since-activation__heading">TIME SINCE ACTIVATION:</span>
-            <span className="body__countdown-container__time-since-activation__clock">{formatTime(validityTimer)}</span>
-          </div>
-        </div>
-      </div>
-
-
-      <div id="footer" className="footer green--flashing">
-        <span className="footer__instruction">Please show this to the proper authority on board the train.</span>
-        <span className="footer__hourglass">{formatTime(four_hours_as_seconds - validityTimer)}</span>
-      </div>
-    </div>
-  );
+    );
+  } 
 }
 
 function padDigits(num) {
